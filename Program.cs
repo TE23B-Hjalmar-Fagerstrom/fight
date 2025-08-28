8ed3bbfed727a078b4d3de3c5e53886f4709ca04
Hjalmar Fagerström <hjalmar.fagerstrom@elev.ga.ntig.se>
2025-08-28 12:46:51 +0200

    "3" förbättringar gjorda

--- Program.cs
+++ Program.cs
@@ -1,29 +1,34 @@
-﻿
-while (true)
+﻿Console.WriteLine("välkommen till fightclub");
+Console.WriteLine("tryck ENTER för att starta");
+
+Console.ReadLine();
+
+Console.WriteLine("Vad heter du?");
+string playerName = Console.ReadLine();
+
+bool playAgian = true;
+
+while (playAgian == true)
 {
-    Game();
+    Game(ref playAgian, ref playerName);
 }
 
-static void Game()
+static void Game(ref bool playAgian, ref string playerName)
 {
+
 
     int fighter1HP = 100;
     int fighter1ATK = Random.Shared.Next(5, 15);
     int fighter2HP = 100;
     int fighter2ATK = Random.Shared.Next(5, 15);
 
-    Console.WriteLine("välkommen till fightclub");
-
-    Console.WriteLine("tryck ENTER för att starta");
-
-    Console.ReadLine();
 
     while (fighter1HP > 0 && fighter2HP > 0)
     {
         fighter1HP -= fighter2ATK;
         fighter2HP -= fighter1ATK;
 
-        Console.WriteLine($"fighetr 1 tog {fighter2ATK} skada och har {fighter1HP} liv kvar");
+        Console.WriteLine($"{playerName} tog {fighter2ATK} skada och har {fighter1HP} liv kvar");
         Console.WriteLine($"fighetr 2 tog {fighter1ATK} skada och har {fighter2HP} liv kvar");
 
         fighter1ATK = Random.Shared.Next(5, 15);
@@ -35,7 +40,7 @@ static void Game()
     if (fighter1HP > 0)
 
     {
-        Console.WriteLine("fighter 1 Van");
+        Console.WriteLine($"{playerName} Van");
     }
     else if (fighter2HP > 0)
     {
@@ -45,5 +50,23 @@ static void Game()
     {
         Console.WriteLine("ingen Van");
     }
-    Console.ReadLine();
+
+    Console.WriteLine("Vill du spela igen? Y/N");
+    string agian = Console.ReadLine();
+
+    while (agian != "Y" && agian != "N")
+    {
+        Console.WriteLine("Skriv ett stort Y eller N");
+        agian = Console.ReadLine();
+    }
+
+    if (agian == "Y")
+    {
+        playAgian = true;
+    }
+    else if (agian == "N")
+    {
+        playAgian = false;
+    }
+
 }
